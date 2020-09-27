366c25f1f05cc73afaf67da94340c9dc9c49afe1
Ian Costanzo <ian@anon-solutions.ca>
2020-09-26 18:07:50 -0700

    Fix port numbers
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- run_audit.sh
+++ run_audit.sh
@@ -5,9 +5,9 @@ export LEDGER_URL=http://${DOCKERHOST}:9000
 sleep 5
 
 ./run_docker start \
- --endpoint http://${DOCKERHOST}:8020 \
- --inbound-transport http 0.0.0.0 8020 \
- --outbound-transport http --admin 0.0.0.0 8021 \
+ --endpoint http://${DOCKERHOST}:7020 \
+ --inbound-transport http 0.0.0.0 7020 \
+ --outbound-transport http --admin 0.0.0.0 7021 \
  --label Faber.Agent \
  --auto-ping-connection \
  --auto-respond-messages \
@@ -18,9 +18,6 @@ sleep 5
  --wallet-name audit.agent390822 \
  --wallet-key audit.Agent390822 \
  --preserve-exchange-records \
- --plugin aca-py-audit-plugin \
- --plugin aca_py_audit_plugin \
- --plugin aca-py-audit-proof \
  --plugin aca_py_audit_proof \
  --genesis-url ${LEDGER_URL}/genesis \
  --trace-target log \
